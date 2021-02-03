79cbc210597d83bfe70fa0e71b3fcd660323640d
brenoangelo <brenoangeloal@gmail.com>
2021-02-03 15:04:41 -0300

    Estilização

---
+++ churrascometro/js/script.js
@@ -0,0 +1,18 @@
+/*
+
+2h de duração
+400g de carne por pessoa
+4 latas de cerveja
+1 litro e meio de refrigerante
+1 pão de alho
+crianças podem ser contabilizados como meia pessoa.
+
+
+4h de duração
+800g de carne por pessoa
+8 latas de cerveja
+3 litros de refrigerante
+2 pães de alhos
+crianças podem ser contabilizados como meia pessoa.
+*/
+
